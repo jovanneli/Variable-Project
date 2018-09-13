ba5e976776ad8f0e4b1b8a3338edaf439d57152d
Jovi <42977182+jovanneli@users.noreply.github.com>
2018-09-13 13:41:17 -0400

    Add files via upload

---
+++ src/VariablesHW.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ * @author jovanne li
+ *
+ */
+public class VariablesHW 
+{
+
+	public static void main(String[] args) 
+	{String name="My name is Jovanne";
+	 System.out.println(name);
+	 
+	 int length=3;
+	 int width=4;
+	 int area=length*width;
+	 System.out.println(area);
+	 
+	 int radius=4;
+	 double pi=3.14;
+	 double circlearea=pi*radius*radius;
+	 System.out.println(circlearea);
+		
+
+	}
+
+}
